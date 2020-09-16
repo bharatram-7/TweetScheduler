d323362c2506e715092b2004e9ef6c0a6d68795d
Bharatram <bharatram11@gmail.com>
2020-09-16 15:02:25 +0530

    Adding smtp

--- Scheduler/settings.py
+++ Scheduler/settings.py
@@ -132,14 +132,15 @@ CRONJOBS = [
     ('* * * * *', 'twitter.cron.fetch_records', '>> /tmp/pycharm_crontab.log 2>&1')
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
-SESSION_COOKIE_SECURE = True
-
-CSRF_COOKIE_SECURE = True
-
-SECURE_SSL_REDIRECT = True
